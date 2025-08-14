f533e49014724e4dd247f42cc1e161f8cb0566ec
Shobhit010 <shobhitpoddar410@gmail.com>
2025-08-15 02:00:32 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,73 @@
+import { StatusBar } from 'expo-status-bar';
+import { Alert, StyleSheet, Text, View, Alert } from 'react-native';
+import React, { useEffect } from 'react';
+
+export default function App() {
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  useEffect(() => {
+    if (requestUserPermission()) {
+      // return fcm token for the device 
+      messaging().getToken().then(token => {
+        console.log('FCM Token:', token);
+      });
+    } 
+    else {
+      console.log('Failed token status', authStatus);
+    }
+
+    // Check whether an initial notification is available
+    messaging()
+    .getInitialNotification()
+    .then( async (remoteMessage) => {
+      if (remoteMessage) {
+        console.log('Notification caused app to open from quit state:', remoteMessage.notification);
+      }
+      setLoading(false);
+    });
+
+    //Assume a message-noitification contains a "type" property in the data payload of the screen to open
+
+    messaging().onNotificationOpenedApp( async (remoteMessage) => {
+      console.log('Notification caused app to open from background state:', remoteMessage.notification);
+    });
+
+    // Register background handler
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage);
+    });
+
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+
+
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <Text>FCM Push Notification Integration Using React Native & Expo</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
